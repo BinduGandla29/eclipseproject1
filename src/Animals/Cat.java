7998741cbc6427ae5645fff596b748084c9a6c23
HimaBindu <kannam4261@saskpolytech.ca>
2022-03-12 10:22:24 -0500

    eclipse code commit

---
+++ src/Animals/Cat.java
@@ -0,0 +1,10 @@
+package Animals;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Vegetables/Carrot.java
@@ -0,0 +1,10 @@
+package Vegetables;
+
+public class Carrot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
